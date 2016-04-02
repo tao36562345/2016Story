59f3354b69777ec362d92d769f911b396f6d4812
badman <dengtao@bad.local>
2016-04-02 22:16:59 +0800

    urlScheme的demo

---
+++ UrlSchemeDemo/UrlSchemeDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  UrlSchemeDemo
+//
+//  Created by badman on 16/4/2.
+//  Copyright © 2016年 dengtao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
